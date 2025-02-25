483e12a1eac705e22c5d4d4c2a28010a890ceef4
Sabil49 <111126146+Sabil49@users.noreply.github.com>
2025-02-25 02:06:20 -1200

    Update page.tsx

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -87,8 +87,8 @@ export default function Dashboard(){
                         <div className=" rounded-full w-8 h-8 bg-slate-200 ml-4"></div>
                     </div>
                 </div>
-                <div className=" bg-white p-2">
-                <div className="*:w-[47.25%] gap-y-2 lg:gap-y-0 lg:*:w-1/4 flex flex-wrap lg:flex-nowrap rounded-md *:p-4 gap-x-2 *:rounded-md  *:border border-[rgb(229 229 229)]">
+                <div className=" bg-white p-1">
+                <div className="*:w-[47.5%] gap-y-2 lg:gap-y-0 lg:*:w-1/4 flex flex-wrap lg:flex-nowrap rounded-md *:p-4 gap-x-2 *:rounded-md  *:border border-[rgb(229 229 229)]">
                     <div>
                         <b className=" ">Total Users</b>
                         <p className="text-xs text-gray-400">The total number of registered users till now.</p>
@@ -124,7 +124,7 @@ export default function Dashboard(){
                 </div>
                 </div>
 
-                <div className="p-4 !pt-0 gap-x-4 gap-y-4 lg:gap-y-0 *:w-full lg:*:w-[32.33%] flex flex-wrap lg:flex-nowrap *:border lg:*:h-[400px] bg-white">
+                <div className="p-1 !pt-0 gap-x-4 gap-y-4 lg:gap-y-0 *:w-full lg:*:w-[32.33%] flex flex-wrap lg:flex-nowrap *:border lg:*:h-[400px] bg-white">
                     <div> 
                     <div className=' px-8 pt-4'><b className=" ">Sales Trends</b>
                         <p className="text-xs text-gray-400">A line chart showing sales trends over time.</p>
